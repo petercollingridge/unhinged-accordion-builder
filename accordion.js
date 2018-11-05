bccd367609c6471c4014fa44f825bf9054bcb6c2
Peter Collingridge <peter.collingridge@gmail.com>
2018-11-05 21:19:24 +0000

    Show cuts as splits in outer paper shape rather than lines

--- accordion.js
+++ accordion.js
@@ -3,7 +3,7 @@ var app = new Vue({
     data: {
         width: 76,
         height: 56,
-        columns: 8,
+        columns: 4,
         rows: 3,
         paperType: 'custom',
         paperTypes: {
@@ -46,19 +46,45 @@ var app = new Vue({
             var d = "M0 0";
 
             // Top folded line
-            for (var i = 1; i <= this.columns; i++) {
-                d += " L" + this.getX(i) + " " + this.getFoldY(i, 0);
+            d += this.getFoldLine(1, this.columns, 0);
+
+            // Right hand side with cuts
+            var cutSize = 2;
+
+            for (var j = 1; j < this.rows; j++) {
+                var y = this.getFoldY(this.columns, j);
+
+                // Cut along x
+                if ((this.rows - j) % 2 === 0) {
+                    d += " V" + (y - cutSize);
+                    d += this.getFoldLine(this.columns, 1, j, 0, -cutSize);
+                    d += this.getFoldLine(1, this.columns, j, cutSize, 0);
+                } else {
+                    d += " V" + y;
+                }
             }
 
             d += " V" + this.getFoldY(this.columns, this.rows);
 
             // Bottom folded line
-            for (var i = this.columns; i >= 0; i--) {
-                d += " L" + this.getX(i) + " " + this.getFoldY(i, this.rows);
+            d += this.getFoldLine(this.columns, 0, this.rows);
+
+            // Left hand side with cuts
+            for (j = this.rows - 1; j > 0; j--) {
+                var y = this.getFoldY(this.columns, j);
+
+                // Cut along x
+                if ((this.rows - j) % 2 === 1) {
+                    d += " V" + (y + cutSize);
+                    d += this.getFoldLine(0, this.columns - 1, j, 0, cutSize);
+                    d += this.getFoldLine(this.columns - 1, 0, j, -cutSize, 0);
+                } else {
+                    d += " V" + y;
+                }
             }
 
             return d + "z";
-        }
+        },
     },
     methods: {
         getX: function(i) {
@@ -70,16 +96,27 @@ var app = new Vue({
         getFoldY: function(x, y) {
             return 0.5 + y * this.paperHeight / this.rows + (x % 2) * 8;
         },
+        getFoldLine: function(x1, x2, y, y1, y2) {
+            var s = "";
+            y1 = y1 || 0;
+            y2 = y2 || 0;
+            
+            if (x1 < x2) {
+                for (var x = x1; x <= x2; x++) {
+                    var p = (x - x1) / (x2 - x1);
+                    s += " L" + this.getX(x) + " " + (this.getFoldY(x, y) + p * y1 + (1 - p) * y2);
+                }
+            } else {
+                for (var x = x1; x >= x2; x--) {
+                    var p = (x - x1) / (x2 - x1);
+                    s += " L" + this.getX(x) + " " + (this.getFoldY(x, y) + p * y1 + (1 - p) * y2);
+                }
+            }
+
+            return s;
+        },
         foldType: function(i) {
             return i % 2 ? { 'hill-fold': true }: { 'valley-fold': true };
-        },
-        foldOrCut: function(x, y) { 
-            if ((x === 0 && (this.rows - y) % 2 === 0) ||
-                (x === this.columns - 1 && (this.rows - y) % 2 === 1)
-            ) {
-                return 'hill-fold';
-            }
-            return 'cut-line';
         }
     }
 });
